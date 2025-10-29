04fd8f6d26ae82ba9f7270d687ceb4f07142839e
Augusto <augustopreisthomasi@gmail.com>
2025-10-29 19:25:46 -0300

    implementa adapter e bridge

--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,54 @@
-console.log("Hello, World! 002");
+class Device {
+  turnOn() {}
+  turnOff() {}
+  setVolume(level) {}
+}
+
+class TV extends Device {
+  turnOn() {
+    console.log("TV ligada.");
+  }
+
+  turnOff() {
+    console.log("TV desligada.");
+  }
+}
+
+class Radio extends Device {
+  turnOn() {
+    console.log("Rádio ligado.");
+  }
+
+  turnOff() {
+    console.log("Rádio desligado.");
+  }
+}
+
+class RemoteControl {
+  constructor(device) {
+    this.device = device;
+  }
+
+  pressPowerButton(on) {
+    on ? this.device.turnOn() : this.device.turnOff();
+  }
+}
+
+class AdvancedRemoteControl extends RemoteControl {
+  mute() {
+    console.log("Dispositivo no modo mudo.");
+  }
+}
+
+const tv = new TV();
+const remoteForTV = new RemoteControl(tv);
+
+remoteForTV.pressPowerButton(true);
+remoteForTV.pressPowerButton(false);
+
+const radio = new Radio();
+const advancedRemote = new AdvancedRemoteControl(radio);
+
+advancedRemote.pressPowerButton(true);
+advancedRemote.mute();
+advancedRemote.pressPowerButton(false);
